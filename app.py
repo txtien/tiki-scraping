c91b9df07e4eec5de551ba9c841350806f084a7f
txtien <tranxuantien8414@gmail.com>
2019-11-22 21:42:11 +0700

    final

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
 from flask import Flask,render_template
 import sql
+import scrap
+import requests 
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    categories = sql.get_categories()
-    return render_template('index.html',categories=categories)
+def index():
+    categories = scrap.category_link()
+    d = dict()
+    for name, path in categories.items():
+        d[name] = scrap.scrap_img(path)
+    return render_template('sub_cate.html',categories=categories, img_dict = d)
+
+@app.route('/<path:name>')
+def test(name):
+    path = 'https://tiki.vn/' + name
+
+    cate_nho = sql.check_category(path)
+
+    productlist = []
+    for names in cate_nho:
+        a = sql.get_product(names)
+        for product in a:
+            productlist.append(product[0])
+    
+    if cate_nho:
+        return render_template('product_in_category.html', productlist=productlist)
+
+        
+    else:
+        categories = scrap.subcategory_link(name)
+        d = dict()
+        for name, path in categories.items():
+            d[name] = scrap.scrap_img(path)
+        return render_template('sub_cate.html', categories=categories, img_dict=d)
+
+
+# @app.route('/<path:product_path>')
+# def get_product(product_path):
+#     return render_template('product.html', product_path=product_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
--- sql.py
+++ sql.py
@@ -13,3 +13,24 @@ def get_categories():
     categories = cursor.fetchall()
 
     return categories
+
+
+def check_category(name):
+    cursor.execute("SELECT DISTINCT c.url, pcs.cate_lon FROM pcs JOIN categories as c ON pcs.cat_id = c.id;")
+    check_list = cursor.fetchall()
+    
+    name_list = []
+    for item in check_list:
+        if name == item[0]:
+            name_list.append(item[1])
+    return name_list
+
+
+def get_product(name):
+    cursor.execute(f"SELECT p.name FROM pcs JOIN products as p ON pcs.cat_id = p.cat_id WHERE cate_lon LIKE '{name}';")
+    product_list = cursor.fetchall()
+
+    return product_list
+
+
+
